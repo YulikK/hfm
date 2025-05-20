58cfc8e39006253f65437f544832a890d681c5c1
YulikK <yulikpoch@gmail.com>
2025-05-20 23:34:03 +0200

    feat: add registration form component and related assets

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Header from './components/header/header';
+import RegistrationForm from './components/registration-form/registration-form';
 import './styles.scss';
 
 const { body } = document;
 
 const header = new Header();
+const form = new RegistrationForm();
 body.appendChild(header.getNode());
+body.appendChild(form.getNode());
