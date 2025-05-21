f3d6aae0eba4011ea96ec6e9d02a5469608c20ef
YulikK <yulikpoch@gmail.com>
2025-05-21 14:56:22 +0200

    refactor: reorganize registration form and join button styles

--- src/main.js
+++ src/main.js
@@ -2,19 +2,16 @@ import FAQ from './components/faq/faq';
 import Features from './components/features/features';
 import Footer from './components/footer/footer';
 import Prizes from './components/prizes/prizes';
-import RegistrationForm from './components/registration-form/registration-form';
 import Winners from './components/winners/winners';
 import './styles.scss';
 
 const { body } = document;
 
-const form = new RegistrationForm();
 const feat = new Features();
 const prizes = new Prizes();
 const winners = new Winners();
 const faq = new FAQ();
 const footer = new Footer();
-body.appendChild(form.getNode());
 body.appendChild(feat.getNode());
 body.appendChild(prizes.getNode());
 body.appendChild(winners.getNode());
