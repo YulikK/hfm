537d7575f01d86ded775c86ce9971f7264d2514f
YulikK <yulikpoch@gmail.com>
2025-05-21 19:52:17 +0200

    feat: enhance form validation and mobile menu functionality

--- src/main.js
+++ src/main.js
@@ -1,37 +1,3 @@
+import './app/form.js';
+import './app/mobileMenu.js';
 import './styles.scss';
-
-const MOBILE_MENU_VIEW = 1020;
-
-document.addEventListener('DOMContentLoaded', () => {
-  const { body } = document;
-  const burgerButton = body.querySelector('#burger');
-  const mobileMenu = body.querySelector('#mobile-menu');
-  const closeButton = body.querySelector('#close');
-  const overlay = document.querySelector('.mobile-menu-overlay');
-
-  const toggleMenu = () => {
-    mobileMenu.classList.toggle('is-open');
-    overlay.classList.toggle('is-active');
-    document.body.classList.toggle('no-scroll');
-  };
-
-  const closeMenu = () => {
-    mobileMenu.classList.remove('is-open');
-    overlay.classList.remove('is-active');
-    document.body.classList.remove('no-scroll');
-  };
-
-  const handleResize = () => {
-    if (
-      window.innerWidth > MOBILE_MENU_VIEW &&
-      mobileMenu.classList.contains('is-open')
-    ) {
-      closeMenu();
-    }
-  };
-
-  burgerButton.addEventListener('click', toggleMenu);
-  closeButton.addEventListener('click', toggleMenu);
-  overlay.addEventListener('click', toggleMenu);
-  window.addEventListener('resize', handleResize);
-});
