7fae3e774a5dab28f54ad071e119165f75356492
YulikK <yulikpoch@gmail.com>
2025-05-21 15:44:24 +0200

    refactor: move faq to html

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
-import FAQ from './components/faq/faq';
 import Footer from './components/footer/footer';
 import './styles.scss';
 
 const { body } = document;
 
-const faq = new FAQ();
 const footer = new Footer();
 
-body.appendChild(faq.getNode());
 body.appendChild(footer.getNode());
