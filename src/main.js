947d7a60e8ba6792dbce1e6fa44a846b8cb8fc2c
YulikK <yulikpoch@gmail.com>
2025-05-21 15:39:04 +0200

    refactor: move winners component to html

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import FAQ from './components/faq/faq';
 import Footer from './components/footer/footer';
-import Winners from './components/winners/winners';
 import './styles.scss';
 
 const { body } = document;
 
-const winners = new Winners();
 const faq = new FAQ();
 const footer = new Footer();
 
-body.appendChild(winners.getNode());
 body.appendChild(faq.getNode());
 body.appendChild(footer.getNode());
