979dee89aacadd589c6a51239bfce6275b7ff9d8
YulikK <yulikpoch@gmail.com>
2025-05-21 15:03:10 +0200

    refactor: move features component to global styles and HTML

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import FAQ from './components/faq/faq';
-import Features from './components/features/features';
 import Footer from './components/footer/footer';
 import Prizes from './components/prizes/prizes';
 import Winners from './components/winners/winners';
@@ -7,12 +6,10 @@ import './styles.scss';
 
 const { body } = document;
 
-const feat = new Features();
 const prizes = new Prizes();
 const winners = new Winners();
 const faq = new FAQ();
 const footer = new Footer();
-body.appendChild(feat.getNode());
 body.appendChild(prizes.getNode());
 body.appendChild(winners.getNode());
 body.appendChild(faq.getNode());
