d060783c62b2b9bf271f127f8d36bb22aefaa1f2
YulikK <yulikpoch@gmail.com>
2025-05-21 13:32:29 +0200

    feat: add footer component with social links and app download options

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import FAQ from './components/faq/faq';
 import Features from './components/features/features';
+import Footer from './components/footer/footer';
 import Header from './components/header/header';
 import Prizes from './components/prizes/prizes';
 import RegistrationForm from './components/registration-form/registration-form';
@@ -14,9 +15,11 @@ const feat = new Features();
 const prizes = new Prizes();
 const winners = new Winners();
 const faq = new FAQ();
+const footer = new Footer();
 body.appendChild(header.getNode());
 body.appendChild(form.getNode());
 body.appendChild(feat.getNode());
 body.appendChild(prizes.getNode());
 body.appendChild(winners.getNode());
 body.appendChild(faq.getNode());
+body.appendChild(footer.getNode());
